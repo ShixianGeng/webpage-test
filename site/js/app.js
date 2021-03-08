069eb92bcceb4238492d35b19f363c5b015cf72f
Shixian <shixiangeng@gmail.com>
2021-03-08 22:02:23 +0900

    add video part

--- site/js/app.js
+++ site/js/app.js
@@ -7,11 +7,17 @@ nameinput.addEventListener('submit',updateName);
 
 for (var i = 1; i <=22; i++) {
 	var form = document.getElementById("form"+i);
-	form.addEventListener('submit', uploadFile(i));
+	form.addEventListener('submit', uploadPhoto(i));
+}
+
+for (var k = 1; k <=8; k++) {
+	var form = document.getElementById("form2_"+k);
+	form.addEventListener('submit', uploadVideo(k));
 }
 
 
-function uploadFile(arg){
+
+function uploadPhoto(arg){
 	return function curried_function(event){
 		if(name === ""){
 			alert("You must input a name!");
@@ -20,6 +26,24 @@ function uploadFile(arg){
 			var ref = storageRef.child(name+'/pic'+arg);
 			ref.put(file).then((snapshot) =>{
 			alert("submitted "+"photo #"+arg);
+			});
+			event.preventDefault();
+
+		}
+
+
+	}
+}
+
+function uploadVideo(arg){
+	return function curried_function(event){
+		if(name === ""){
+			alert("You must input a name!");
+		}else{
+			var file = document.getElementById("vid"+arg).files[0];
+			var ref = storageRef.child(name+'/video'+arg);
+			ref.put(file).then((snapshot) =>{
+			alert("submitted "+"video #"+arg);
 			});
 			event.preventDefault();
 
