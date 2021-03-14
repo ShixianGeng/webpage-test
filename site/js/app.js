413cac3e754db5823f96471a1f21d201aa8ebbeb
Shixian <shixiangeng@gmail.com>
2021-03-15 03:54:12 +0900

    new instruction after meeting

--- site/js/app.js
+++ site/js/app.js
@@ -5,45 +5,23 @@ var name;
 var nameinput = document.getElementById("nameinput");
 nameinput.addEventListener('submit',updateName);
 
-for (var i = 1; i <=22; i++) {
-	var form = document.getElementById("form"+i);
-	form.addEventListener('submit', uploadPhoto(i));
-}
-
-for (var k = 1; k <=8; k++) {
-	var form = document.getElementById("form2_"+k);
-	form.addEventListener('submit', uploadVideo(k));
-}
+var form = document.getElementById("form");
+form.addEventListener('submit', uploadVideo());
 
 
 
-function uploadPhoto(arg){
+function uploadVideo(){
 	return function curried_function(event){
 		if(name === ""){
 			alert("You must input a name!");
 		}else{
-			var file = document.getElementById("pic"+arg).files[0];
-			var ref = storageRef.child(name+'/pic'+arg);
+			var file = document.getElementById("vid").files[0];
+			var ref = storageRef.child(name+'/');
+			var upload = document.getElementById("upload");
+			upload.innerText = "Uploading... Please Wait."
 			ref.put(file).then((snapshot) =>{
-			alert("submitted "+"photo #"+arg);
-			});
-			event.preventDefault();
-
-		}
-
-
-	}
-}
-
-function uploadVideo(arg){
-	return function curried_function(event){
-		if(name === ""){
-			alert("You must input a name!");
-		}else{
-			var file = document.getElementById("vid"+arg).files[0];
-			var ref = storageRef.child(name+'/video'+arg);
-			ref.put(file).then((snapshot) =>{
-			alert("submitted "+"video #"+arg);
+			alert("Submitted video. Thank you very much!");
+			upload.innerText = "Uploaded"
 			});
 			event.preventDefault();
 
