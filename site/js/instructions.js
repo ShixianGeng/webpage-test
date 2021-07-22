27f1e90297e1f2cb0f966d7c20caf599cb0850af
Shixian <shixiangeng@gmail.com>
2021-07-23 05:24:19 +0900

    test download

--- site/js/instructions.js
+++ site/js/instructions.js
@@ -1,31 +1,31 @@
 // Create a root reference
 var name = window.location.href.split('=')[1];
-var storageRef = firebase.storage().ref();
-var form = document.getElementById("form");
-form.addEventListener('submit', uploadVideo());
+// var storageRef = firebase.storage().ref();
+// var form = document.getElementById("form");
+// form.addEventListener('submit', uploadVideo());
 var id_indicator = document.getElementById("id_indicator");
 id_indicator.innerText = name;
 
-function uploadVideo(){
-	return function curried_function(event){
-		if(name === ""){
-			alert("あなたの番号を入力してください.");
-		}else{
-			var file = document.getElementById("vid").files[0];
-			var ref = storageRef.child(name+'/'+Date.now()+'/');
-			var upload = document.getElementById("upload");
-			upload.innerText = "アップロード中です...ブラウザを閉じないでください."
-			ref.put(file).then((snapshot) =>{
-			alert("アップロード完了.ありがとうございました.");
-			upload.innerText = "アップロード完了. ありがとうございました."
-			});
-			event.preventDefault();
+// function uploadVideo(){
+// 	return function curried_function(event){
+// 		if(name === ""){
+// 			alert("あなたの番号を入力してください.");
+// 		}else{
+// 			var file = document.getElementById("vid").files[0];
+// 			var ref = storageRef.child(name+'/'+Date.now()+'/');
+// 			var upload = document.getElementById("upload");
+// 			upload.innerText = "アップロード中です...ブラウザを閉じないでください."
+// 			ref.put(file).then((snapshot) =>{
+// 			alert("アップロード完了.ありがとうございました.");
+// 			upload.innerText = "アップロード完了. ありがとうございました."
+// 			});
+// 			event.preventDefault();
 
-		}
+// 		}
 
 
-	}
-}
+// 	}
+// }
 
 
 
