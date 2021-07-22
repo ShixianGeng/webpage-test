584240c4e1e446ce1c4a7a3717c176d6fbea5dc7
Shixian <shixiangeng@gmail.com>
2021-07-23 05:36:01 +0900

    test download

--- site/js/app.js
+++ site/js/app.js
@@ -5,7 +5,7 @@ nameinput.addEventListener('submit',updateName);
 function updateName(event){
 	name = document.getElementById("username").value;
 	if(name === ""){
-		alert("あなたの番号を入力してください.");
+		alert("番号を入力してください.");
 	}else{
 		alert("番号が更新されました.");
 		window.location.href = "./instructions.html?id="+name;
